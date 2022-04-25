e8b809a771064e9a840b1e03e83f13fee9d31078
Dhanalaxmi Otari <dhanalaxmi.otari@instem.com>
2022-04-25 15:54:52 +0530

    Code

---
+++ Programs_String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Programs_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textWithCountryNames = "Afghanistan,Ecuador,Albania,Italy,Algeria,Gabon,Andorra,Belgium,Angola,Antigua,Jamaica,Argentina,France,Armenia,Australia,Jordan,Austria,Barbados,Belarus";
+
+            Regex regex = new Regex(@"\b[A]\w+");
+            MatchCollection countryMatch = regex.Matches(textWithCountryNames);
+
+            foreach (Match match in countryMatch)
+            {
+                Console.WriteLine("Country name : {0}, Index : {1}", match.Value, match.Index);
+            }
+
+        }
+    }
+}
