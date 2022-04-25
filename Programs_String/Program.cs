7a8a37033832eb03ebf52c1570574abeca3a42de
Dhanalaxmi Otari <dhanalaxmi.otari@instem.com>
2022-04-25 18:42:19 +0530

    Two programs comitted

--- Programs_String/Program.cs
+++ Programs_String/Program.cs
@@ -5,17 +5,38 @@ namespace Programs_String
 {
     class Program
     {
+        //find missing integer in an array
+        static int arraySum;
+        int mean = 9;
+        public int MissingIntegerInArray() 
+        {
+            int[] array = { 2, 4, 20 };
+
+            for(int item =0; item < array.Length; item++)
+            {
+                arraySum += array[item];
+                //Console.WriteLine(arraySum);
+            }
+            int missingElement =(9*4) - arraySum;
+            //Console.WriteLine(missingElement);
+            return missingElement;
+        }
+
+
         static void Main(string[] args)
         {
             string textWithCountryNames = "Afghanistan,Ecuador,Albania,Italy,Algeria,Gabon,Andorra,Belgium,Angola,Antigua,Jamaica,Argentina,France,Armenia,Australia,Jordan,Austria,Barbados,Belarus";
 
-            Regex regex = new Regex(@"\b[A]\w+");
-            MatchCollection countryMatch = regex.Matches(textWithCountryNames);
+            Regex regexA = new Regex(@"\b[A]\w+");
+            Regex regexI = new Regex(@"\b[I]\w+");
+            MatchCollection countryMatch = regexI.Matches(textWithCountryNames);
 
             foreach (Match match in countryMatch)
             {
                 Console.WriteLine("Country name : {0}, Index : {1}", match.Value, match.Index);
             }
+            Program test = new Program();
+            Console.WriteLine(test.MissingIntegerInArray());
 
         }
     }
