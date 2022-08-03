1e193186b4c23425d46f80c381b44f254cd6e4d4
Dhanalaxmi Otari <dhanalaxmi.otari@instem.com>
2022-08-03 12:47:07 +0530

    Code Change

--- Programs_String/Program.cs
+++ Programs_String/Program.cs
@@ -9,37 +9,44 @@ namespace Programs_String
         public int CountDuplicateOccuranceElementInArray()
         {
             string myString = "passage";
-            char myChar;
+            char[] myChar = myString.ToCharArray();
 
-                int count = 0;
-                for (int i = 0; i < myString.Length; i++)
+            int count = 0;
+            for (int i = 0; i < myString.Length; i++)
+            {
+                for (int j = 1; j < myString.Length; j++)
                 {
-                if (myString[i] == myChar)
-                {
-                    ++count;
+                    count = 0;
+                    if (myChar[i] == myChar[j])
+                    {
+                        ++count;
+                        Console.WriteLine(count);
+                    }
                 }
-                }
-                return count;
+
+            }
             
+            return count;
+
         }
         //find missing integer in an array
         static int arraySum;
         int mean = 9;
-        public int MissingIntegerInArray() 
+        public int MissingIntegerInArray()
         {
             int[] array = { 2, 4, 20 };
 
-            for(int item =0; item < array.Length; item++)
+            for (int item = 0; item < array.Length; item++)
             {
                 arraySum += array[item];
                 //Console.WriteLine(arraySum);
             }
-            int missingElement =(9*4) - arraySum;
+            int missingElement = (9 * 4) - arraySum;
             //Console.WriteLine(missingElement);
             return missingElement;
         }
-       // print numbers without loop
-       public static void PrintNumbers()
+        // print numbers without loop
+        public static void PrintNumbers()
         {
             try
             {
@@ -50,19 +57,19 @@ namespace Programs_String
                 {
                     Console.WriteLine(i);
                     goto begine;
-                    
+
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
-            
+
         }
 
         static void Main(string[] args)
         {
-            string textWithCountryNames = "Afghanistan,Ecuador,Albania,Italy,Algeria,Gabon,Andorra,Belgium,Angola,Antigua,Jamaica,Argentina,France,Armenia,Australia,Jordan,Austria,Barbados,Belarus";
+            /*string textWithCountryNames = "Afghanistan,Ecuador,Albania,Italy,Algeria,Gabon,Andorra,Belgium,Angola,Antigua,Jamaica,Argentina,France,Armenia,Australia,Jordan,Austria,Barbados,Belarus";
 
             Regex regexA = new Regex(@"\b[A]\w+");
             Regex regexI = new Regex(@"\b[I]\w+");
@@ -71,11 +78,11 @@ namespace Programs_String
             foreach (Match match in countryMatch)
             {
                 Console.WriteLine("Country name : {0}, Index : {1}", match.Value, match.Index);
-            }
+            }*/
             Program test = new Program();
-            Console.WriteLine(test.MissingIntegerInArray());
-            Program.PrintNumbers();
-
+            //Console.WriteLine(test.MissingIntegerInArray());
+            //Program.PrintNumbers();
+            test.CountDuplicateOccuranceElementInArray();
         }
     }
 }
