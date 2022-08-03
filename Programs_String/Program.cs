eff3ce4213c927ba8633e3cf4cb7446c430d9b6e
Dhanalaxmi Otari <dhanalaxmi.otari@instem.com>
2022-08-03 13:15:56 +0530

    New Duplicate function added

--- Programs_String/Program.cs
+++ Programs_String/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Programs_String
 {
     class Program
     {
+        #region
         //Duplicate char in the string
         public int CountDuplicateOccuranceElementInArray()
         {
             string myString = "passage";
             char[] myChar = myString.ToCharArray();
+
 
             int count = 0;
             for (int i = 0; i < myString.Length; i++)
@@ -20,69 +23,75 @@ namespace Programs_String
                     if (myChar[i] == myChar[j])
                     {
                         ++count;
-                        Console.WriteLine(count);
+
                     }
                 }
 
             }
-            
+
             return count;
 
         }
-        //find missing integer in an array
-        static int arraySum;
-        int mean = 9;
-        public int MissingIntegerInArray()
+        #endregion
+        public void CountDuplicateElementInArray()
         {
-            int[] array = { 2, 4, 20 };
-
-            for (int item = 0; item < array.Length; item++)
-            {
-                arraySum += array[item];
-                //Console.WriteLine(arraySum);
-            }
-            int missingElement = (9 * 4) - arraySum;
-            //Console.WriteLine(missingElement);
-            return missingElement;
+            List<int> elements = new List<int> { 1, 2, 4, 8, -1, 0, 2, 4, 9, 0 };
+            IEnumberable<int> duplicateItems =elements.GroupBy(x=>x).where(x=>)
         }
-        // print numbers without loop
-        public static void PrintNumbers()
+    //find missing integer in an array
+    static int arraySum;
+    int mean = 9;
+    public int MissingIntegerInArray()
+    {
+        int[] array = { 2, 4, 20 };
+
+        for (int item = 0; item < array.Length; item++)
         {
-            try
-            {
-                int i = 0;
-            begine:
-                i = i + 1;
-                if (i < 20)
-                {
-                    Console.WriteLine(i);
-                    goto begine;
-
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
+            arraySum += array[item];
+            //Console.WriteLine(arraySum);
         }
-
-        static void Main(string[] args)
-        {
-            /*string textWithCountryNames = "Afghanistan,Ecuador,Albania,Italy,Algeria,Gabon,Andorra,Belgium,Angola,Antigua,Jamaica,Argentina,France,Armenia,Australia,Jordan,Austria,Barbados,Belarus";
-
-            Regex regexA = new Regex(@"\b[A]\w+");
-            Regex regexI = new Regex(@"\b[I]\w+");
-            MatchCollection countryMatch = regexI.Matches(textWithCountryNames);
-
-            foreach (Match match in countryMatch)
-            {
-                Console.WriteLine("Country name : {0}, Index : {1}", match.Value, match.Index);
-            }*/
-            Program test = new Program();
-            //Console.WriteLine(test.MissingIntegerInArray());
-            //Program.PrintNumbers();
-            test.CountDuplicateOccuranceElementInArray();
-        }
+        int missingElement = (9 * 4) - arraySum;
+        //Console.WriteLine(missingElement);
+        return missingElement;
     }
+    // print numbers without loop
+    public static void PrintNumbers()
+    {
+        try
+        {
+            int i = 0;
+        begine:
+            i = i + 1;
+            if (i < 20)
+            {
+                Console.WriteLine(i);
+                goto begine;
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        /*string textWithCountryNames = "Afghanistan,Ecuador,Albania,Italy,Algeria,Gabon,Andorra,Belgium,Angola,Antigua,Jamaica,Argentina,France,Armenia,Australia,Jordan,Austria,Barbados,Belarus";
+
+        Regex regexA = new Regex(@"\b[A]\w+");
+        Regex regexI = new Regex(@"\b[I]\w+");
+        MatchCollection countryMatch = regexI.Matches(textWithCountryNames);
+
+        foreach (Match match in countryMatch)
+        {
+            Console.WriteLine("Country name : {0}, Index : {1}", match.Value, match.Index);
+        }*/
+        Program test = new Program();
+        //Console.WriteLine(test.MissingIntegerInArray());
+        //Program.PrintNumbers();
+        test.CountDuplicateOccuranceElementInArray();
+    }
+}
 }
